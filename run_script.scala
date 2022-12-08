35cf4949bf5be858944bdad568af2309bb04404a
Richard Harken <rharken@us.ibm.com>
2022-12-07 23:57:25 -0500

    additional read forms

--- run_script.scala
+++ run_script.scala
@@ -1,4 +1,5 @@
 //val df = spark.read.json("./applisttest.json");
+//val df = spark.sql("SELECT id,explode(apps) as apps FROM parquet.`applist.parquet`");
 //val df1 = df.withColumn("app", explode($"apps")).drop($"apps");
 //val df2 = df1.withColumn("appName", $"app.appName").withColumn("appPackage", $"app.appPackage").withColumn("Ratings", explode($"app.Ratings")).drop($"app")
 //df2.groupBy("id", "appName", "appPackage").avg("Ratings.Rating").orderBy("appName").show
